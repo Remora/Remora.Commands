fee74d08fc9f6414e4791a22475fe5a990c374b5
Jarl Gullberg <jarl.gullberg@gmail.com>
2022-04-19 18:15:55 +0200

    Use nuget-provided nullability checking instead of rolling our own.

--- Remora.Commands/Extensions/ParameterInfoExtensions.cs
+++ Remora.Commands/Extensions/ParameterInfoExtensions.cs
@@ -21,8 +21,6 @@
 //
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
 
@@ -35,106 +33,20 @@ namespace Remora.Commands.Extensions;
 public static class ParameterInfoExtensions
 {
     /// <summary>
-    /// Enumerates the various nullability possibilities.
-    /// </summary>
-    private enum Nullability
-    {
-        Oblivious = 0,
-        NotNull = 1,
-        Nullable = 2
-    }
-
-    /// <summary>
     /// Determines whether the given parameter allows null as a value.
     /// </summary>
     /// <param name="parameter">The parameter.</param>
     /// <returns>true if the parameter allows null; otherwise, false.</returns>
     public static bool AllowsNull(this ParameterInfo parameter)
     {
-        var parameterType = parameter.ParameterType;
-        if (parameterType.IsGenericType && parameterType.GetGenericTypeDefinition() == typeof(Nullable<>))
+        var nullabilityInfo = parameter.GetNullabilityInfo();
+
+        return nullabilityInfo.WriteState switch
         {
-            return true;
-        }
-
-        var nullableAttributeType = Type.GetType("System.Runtime.CompilerServices.NullableAttribute");
-        if (nullableAttributeType is null)
-        {
-            // If we don't have access to nullability attributes, assume that we're not in a nullable context.
-            return !parameterType.IsValueType;
-        }
-
-        // We're in a nullable context, and we can assume that the lack of an attribute means the parameter is not
-        // nullable.
-        var nullableAttribute = parameter.CustomAttributes.FirstOrDefault
-        (
-            s => s.AttributeType.FullName == "System.Runtime.CompilerServices.NullableAttribute"
-        );
-
-        var topLevelNullability = Nullability.Oblivious;
-
-        if (nullableAttribute is not null)
-        {
-            var nullableArgument = nullableAttribute.ConstructorArguments.Single();
-            switch (nullableArgument.Value)
-            {
-                case byte singleArg:
-                {
-                    topLevelNullability = (Nullability)singleArg;
-                    break;
-                }
-                case IReadOnlyCollection<CustomAttributeTypedArgument> multiArg:
-                {
-                    if (multiArg.First().Value is not byte firstArg)
-                    {
-                        throw new InvalidOperationException();
-                    }
-
-                    topLevelNullability = (Nullability)firstArg;
-                    break;
-                }
-                default:
-                {
-                    throw new InvalidOperationException();
-                }
-            }
-        }
-
-        switch (topLevelNullability)
-        {
-            case Nullability.Oblivious:
-            {
-                // Check the context instead
-                var nullableContextAttribute = parameter.Member.CustomAttributes.FirstOrDefault
-                (
-                    s => s.AttributeType.FullName == "System.Runtime.CompilerServices.NullableContextAttribute"
-                );
-
-                if (nullableContextAttribute is null)
-                {
-                    return !parameterType.IsValueType;
-                }
-
-                var nullableArgument = nullableContextAttribute.ConstructorArguments.Single();
-                if (nullableArgument.Value is byte singleArg)
-                {
-                    return singleArg == 2;
-                }
-
-                throw new InvalidOperationException();
-            }
-            case Nullability.NotNull:
-            {
-                return false;
-            }
-            case Nullability.Nullable:
-            {
-                return true;
-            }
-            default:
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-        }
+            NullabilityState.Unknown => !parameter.ParameterType.IsValueType,
+            NullabilityState.NotNull => false,
+            NullabilityState.Nullable => true,
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 }
