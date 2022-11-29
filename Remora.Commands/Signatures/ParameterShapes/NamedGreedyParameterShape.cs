fb9d087afacb1fc9cae4119545fef1d17a98c460
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-11-28 23:30:32 -0500

    Fix inconsistency in getting attributes in conditions in NamedGreedyParameterShape

--- Remora.Commands/Signatures/ParameterShapes/NamedGreedyParameterShape.cs
+++ Remora.Commands/Signatures/ParameterShapes/NamedGreedyParameterShape.cs
@@ -158,10 +158,12 @@ public class NamedGreedyParameterShape : IParameterShape
 
     private NamedGreedyParameterShape(ParameterInfo parameter, int index)
     {
+        parameter.GetAttributesAndConditions(out var attributes, out var conditions);
+        
         this.DefaultValue = parameter.DefaultValue;
         this.ParameterType = parameter.ParameterType;
-        this.Attributes = parameter.GetCustomAttributes().Where(a => !typeof(ConditionAttribute).IsAssignableFrom(a.GetType())).ToArray();
-        this.Conditions = parameter.GetCustomAttributes().Where(a => typeof(ConditionAttribute).IsAssignableFrom(a.GetType())).Cast<ConditionAttribute>().ToArray();
+        this.Attributes = attributes;
+        this.Conditions = conditions;
         this.IsNullable = parameter.AllowsNull();
         this._parameterName = parameter.Name;
         this.ParameterIndex = index;
